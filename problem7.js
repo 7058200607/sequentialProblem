142a413f00b6fd114dbef3ab9455dd2f92ab86ae
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-27 19:01:39 +0530

    enter a value and print days

---
+++ problem7.js
@@ -0,0 +1,33 @@
+var readlineSync=require('readline-sync');
+var number = parseInt(readlineSync.question("Enter value : "));
+
+
+switch(number)
+{
+case 0:
+console.log("Sunday"); 
+break;
+case 1:
+console.log("Monday"); 
+break;
+case 2:
+console.log("Tuesday"); 
+break;
+case 3:
+console.log("Wednesday"); 
+break;
+case 4:
+console.log("Thursday");
+break;
+case 5:
+console.log("Friday");
+break;
+case 6:
+console.log("Saturday");
+break;  
+default:
+    console.log("Invalid value");
+}
+    
+
+  
