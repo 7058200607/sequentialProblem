d0dc7a17083cf503e79ff1a88aa2d962255853cc
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-27 16:16:37 +0530

    add Two digit random number and print result

--- problem3.js
+++ problem3.js
@@ -2,9 +2,10 @@
 function addtwoNumber()
 {
 // to gerate a randome rounded number between 1 to 6;
-var theRandomNumber1 = Math.floor((Math.random() * 6) + 1);
-var theRandomNumber2 = Math.floor((Math.random() * 6) + 1);
+var theRandomNumber1 = (Math.floor((Math.random() * 6) + 1));
+var theRandomNumber2 = (Math.floor((Math.random() * 6) + 1));
 var result=theRandomNumber1+theRandomNumber2;
+console.log(result);
 
 }
 addtwoNumber();
