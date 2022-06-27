55ec6e6a49e4e3379df46aac4e953d495ec75ad8
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-27 17:34:49 +0530

    print head or tail

---
+++ problem5.js
@@ -0,0 +1,14 @@
+function headtail()
+{
+
+var toss = Math.random() * 2;
+var floor = Math.floor(toss)
+if(floor === 0){
+    console.log(" Random Coin Value: Head")
+} else if(floor === 1)
+{
+    console.log(" Random Coin Value: Tails")
+}
+}
+headtail();
+
--- singledigit.js
+++ singledigit.js
@@ -1,5 +1,5 @@
 function randIntWithZero() 
 {
-    console.log( Math.floor((Math.random()*6)+1));
+    console.log( Math.floor((Math.random()*9)+1));
 }
 randIntWithZero();
