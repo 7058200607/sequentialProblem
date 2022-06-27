9a6aac4a806f5f62c556d70d5f315493777a02b5
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-27 15:19:14 +0530

    get single digit number

---
+++ problem2.js
@@ -0,0 +1,5 @@
+// Genereates a number between 0 to 1;
+Math.random();
+
+// to gerate a randome rounded number between 1 to 10;
+var theRandomNumber = Math.floor(Math.random() * 10) + 1;
---
+++ singledigit.js
@@ -0,0 +1,5 @@
+function randIntWithZero() 
+{
+    console.log( Math.floor((Math.random()*10)));
+}
+randIntWithZero();
