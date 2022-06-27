c16a91d72734246820078a5b915ce296ce57d95d
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-27 16:45:11 +0530

    print sum and avg of 2 digit random number

--- problem4.js
+++ problem4.js
@@ -1,11 +1,11 @@
 function addtwoNumber()
 {
 // to gerate a randome rounded number between 1 to 6;
-var theRandomNumber1 = (Math.floor((Math.random() * 6) + 1));
-var theRandomNumber2 = (Math.floor((Math.random() * 6) + 1));
-var theRandomNumber3 = (Math.floor((Math.random() * 6) + 1));
-var theRandomNumber4 = (Math.floor((Math.random() * 6) + 1));
-var theRandomNumber5 = (Math.floor((Math.random() * 6) + 1));
+var theRandomNumber1 = (Math.floor((Math.random() * 100) + 10));
+var theRandomNumber2 = (Math.floor((Math.random() * 100) + 10));
+var theRandomNumber3 = (Math.floor((Math.random() * 100) + 10));
+var theRandomNumber4 = (Math.floor((Math.random() * 100) + 10));
+var theRandomNumber5 = (Math.floor((Math.random() * 100) + 10));
 var sum=theRandomNumber1+theRandomNumber2+theRandomNumber3+theRandomNumber4+theRandomNumber5;
 var avg=((theRandomNumber1+theRandomNumber2+theRandomNumber3+theRandomNumber4+theRandomNumber5)/5);
 console.log(sum);
