087228736a5e7cb285b1e9f366fc9f563c11098a
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-27 16:05:42 +0530

    add random number

--- problem2.js
+++ problem2.js
@@ -1,5 +1,7 @@
-// Genereates a number between 0 to 1;
-Math.random();
 
-// to gerate a randome rounded number between 1 to 10;
-var theRandomNumber = Math.floor(Math.random() * 10) + 1;
+function RabdomNumber()
+{
+// to gerate a randome rounded number between 1 to 6;
+console.log( Math.floor(Math.random() * 6) + 1);
+}
+RabdomNumber();
---
+++ problem3.js
@@ -0,0 +1,10 @@
+
+function addtwoNumber()
+{
+// to gerate a randome rounded number between 1 to 6;
+var theRandomNumber1 = Math.floor((Math.random() * 6) + 1);
+var theRandomNumber2 = Math.floor((Math.random() * 6) + 1);
+var result=theRandomNumber1+theRandomNumber2;
+
+}
+addtwoNumber();
